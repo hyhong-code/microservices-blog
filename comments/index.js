c85630d41788b18c048178ea9fd13ca15dcba8e7
Haiyang Hong <haiyanghong@Haiyangs-iMac.local>
2020-06-29 23:13:03 -0700

    Implement event bus endpoint and post to event bus after post and comment creation

--- comments/index.js
+++ comments/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const { randomBytes } = require("crypto");
 const cors = require("cors");
+const axios = require("axios");
 
 const app = express();
 app.use(express.json());
@@ -12,13 +13,19 @@ app.get("/posts/:id/comments", (req, res, next) => {
   res.send(commentsByPostId[req.params.id] || []);
 });
 
-app.post("/posts/:id/comments", (req, res, next) => {
+app.post("/posts/:id/comments", async (req, res, next) => {
   const commentId = randomBytes(4).toString("hex");
   const { content } = req.body;
 
   commentsByPostId[req.params.id] = commentsByPostId[req.params.id]
     ? [...commentsByPostId[req.params.id], { commentId, content }]
     : [{ commentId, content }];
+
+  // POST TO EVENT BUS
+  await axios.post("http://localhost:4005/event", {
+    type: "CommentCreated",
+    data: { id: commentId, content, postId: req.params.id },
+  });
 
   res.status(201).send(commentsByPostId[req.params.id]);
 });
---
+++ event-bus/index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const axios = require("axios");
+
+const app = express();
+app.use(express.json());
+
+app.post("/events", async (req, res, next) => {
+  const event = req.body;
+
+  await axios.post("http://localhost:4000/events", event);
+  await axios.post("http://localhost:4001/events", event);
+  await axios.post("http://localhost:4002/events", event);
+
+  res.send({ status: "OK" });
+});
+
+app.listen(4005, console.log("Event bus listening on port 4005"));
